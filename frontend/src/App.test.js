47c87eefa9ce3cdc941950cc9966468203a80574
Amblem <akhand11092005@gmail.com>
2025-10-12 12:02:00 +0530

    first render test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// first test
+test('renders the initial welcome message from Cern', () => {
+  render(<App />); 
+  
+  const welcomeMessage = screen.getByText(/Hello! Welcome to Regime/i); 
+  
+  expect(welcomeMessage).toBeInTheDocument(); 
 });
+
